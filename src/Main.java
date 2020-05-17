cbcd4509c0ba01877efa2e17240b90fef6e395c0
Mathisfun123 <sairajareddykurelli@gmail.com>
2020-05-16 23:46:55 -0500

    task b done

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,19 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
+		//task b battle
 		Scanner sc = new Scanner(System.in);
-		int s = sc.nextInt(); int w = sc.nextInt();
-		System.out.println(w<s?"safe":"unsafe");
+		int a = sc.nextInt(); int b =sc.nextInt(); int c = sc.nextInt(); int d = sc.nextInt();
+		boolean turn = true;
+		while(a>0&&c>0){
+			if(turn){
+				c-=b;
+			}else{
+				a-=d;
+			}
+			turn= !turn;
+		}
+		System.out.println(a>0?"Yes":"No");
 
 	}
 }
