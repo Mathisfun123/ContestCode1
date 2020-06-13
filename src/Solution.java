4a51908d0730633b454e6e081445506eeb946e25
Mathisfun123 <sairajareddykurelli@gmail.com>
2020-06-13 14:32:52 -0500

    leetcode CoinChange Problem - attempt 2 failed: corrected case where code attempts to use value that was unable to be made before

--- src/Solution.java
+++ src/Solution.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 //leetcode attempts
 public class Solution {
 	public static void main(String[] args) {
-		int[] coins = {1, 2, 5}; int amount= 11;
+		int[] coins = { 2}; int amount= 3;
 		System.out.println(coinChange(coins, amount));
 	}
 	public static int coinChange(int[] coins, int amount) {
@@ -14,12 +14,12 @@ public class Solution {
 		dpcoins[0] = 0;
 		for(int i = 1; i<= amount; i++){
 			for (int coin : coins) {
-				if (i - coin >= 0) {
+				if (i - coin >= 0 && dpcoins[i-coin]!=Integer.MAX_VALUE) {
 					dpcoins[i] = Math.min(dpcoins[i], 1 + dpcoins[i - coin]);
 				}
 			}
 		}
-		return (dpcoins[amount]);
+		return (dpcoins[amount]==Integer.MAX_VALUE)?-1: dpcoins[amount];
 
 	}
 }
