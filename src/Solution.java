e14007507ec9f1de2bd2eb43d9a14efb2d66a5cf
Mathisfun123 <sairajareddykurelli@gmail.com>
2020-06-13 11:44:02 -0500

    leetcode Min Path Sum - attemp1 fail: returns 5 instead of 6 (time to debug)

--- src/Solution.java
+++ src/Solution.java
@@ -3,17 +3,16 @@ import java.util.Arrays;
 //leetcode attempts
 public class Solution {
 	public static void main(String[] args) {
-		int cost[] = {1, 100, 1, 1, 1, 100, 1, 1, 100, 1};
-		System.out.println(minCostClimbingStairs(cost));
+		int[][] grid = {{1,2,5},{3,2,1}};
+		System.out.println(minPathSum(grid));
 	}
-	public static int minCostClimbingStairs(int[] cost) {
-		int[] dps = new int[cost.length+1];
-		dps[0] =cost[0]; dps[1]= cost[1];
-		for(int i = 2; i<=cost.length; i++){
-			dps[i] = Math.min(dps[i-2], dps[i-1]) + (i==cost.length?0:cost[i]);
+	public static int minPathSum(int[][] grid) {
+		int dps[][] = new int[grid.length][grid.length];
+		for(int i = 0; i< grid.length; i++){
+			for(int j =0; j< grid.length; j++){
+				dps[i][j] = (i==0&&j==0)?grid[0][0]: (i==0)? dps[i][j-1] + grid[i][j]: (j==0)? dps[i-1][j]+ grid[i][j] :Math.min(dps[i-1][j], dps[i][j-1]) + grid[i][j];
+			}
 		}
-		return dps[cost.length];
-
-
+		return dps[grid.length-1][grid.length-1];
 	}
 }
